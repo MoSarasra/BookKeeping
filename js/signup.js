ecfdbaef570575e44c7266d22aeb050e44036e6d
wafa395 <wafa.yasser3@gmail.com>
2022-06-30 01:54:58 +0300

    add signup javascript file

---
+++ js/signup.js
@@ -0,0 +1,29 @@
+function submitForm() {
+    const signUpForm = document.getElementById('signup');
+    const userName = signUpForm['username'].value;
+    const email = signUpForm['email'];
+    const password = signUpForm['password'].value;
+    const BusinessName = signUpForm['BusinessName'].value;
+    const country = signUpForm['country'];
+    let countryOptions = country.options[country.selectedIndex].value;
+    const currency = signUpForm['currency'];
+    let currencyOptions = currency.options[currency.selectedIndex].value;
+
+    let newUser = {
+        userName: userName,
+        email: email,
+        password: password,
+        BusinessName: BusinessName,
+        country: countryOptions,
+        currency: currencyOptions
+    }
+
+    let currentUser = JSON.parse(localStorage.getItem('user')) || [];
+    currentUser.push(newUser);
+
+    localStorage.setItem('user', JSON.stringify(currentUser));
+
+}
+
+
+
