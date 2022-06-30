5cdd513498cfc14d8877269a1079f68d5eb361a1
wafa395 <wafa.yasser3@gmail.com>
2022-06-30 22:08:02 +0300

    update addUser functionality localStorage getItem

--- js/adduser.js
+++ js/adduser.js
@@ -1,19 +1,32 @@
 function fullUser() {
-    const userPage = document.getElementById('userNameHtml')
-    
+
     let currentUsers = JSON.parse(localStorage.getItem('user')) || [];
     let userData = ``;
 
     for (const userInfo of currentUsers) {
-        userData +=` ${userInfo.userName} `  ;
+        userData = `Welcome   ${userInfo.userName}` ;
+        userNameHello = userInfo.userName;
     }
     console.log(userData);
 
+    const welcomeName = document.getElementById('name');
+    if(welcomeName){
+        welcomeName.innerText = userData;
+    }
+
+
+
+    const wName = document.getElementById('username');
+    if(wName){
+        wName.innerText = userNameHello ;
+    }
+    
     // document.getElementById(userPage).innerHTML = userData;
     // document.getElementById('userNameHtml').innerHTML = userData;
 
 }
 
+fullUser();
 
 function showdiv() {
 
